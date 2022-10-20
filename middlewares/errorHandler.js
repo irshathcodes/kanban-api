a622544fd9659b217b0f7bb599ea7506c816405b
Irshath <irshath700@gmail.com>
2022-10-20 14:02:28 +0530

    change error message for duplicate values

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -13,7 +13,9 @@ const errorHandler = (err, req, res, next) => {
 	// If client sents registered emails for sign up, this error shows up.
 	if (err.code === 11000) {
 		CustomError.statusCode = 400;
-		CustomError.message = `Cannot register, Because you have already registered with this email : ${err.keyValue.email} please sign in.`;
+		CustomError.message = `duplicate found, no duplicates allowed for this field: ${JSON.stringify(
+			err.keyValue
+		)}`;
 	}
 	res.status(CustomError.statusCode).json({ msg: CustomError.message });
 };
