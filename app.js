7f83a358829529b3bbb550afed1a59c9a6c5afdd
Irshath <irshath700@gmail.com>
2022-10-23 11:50:03 +0530

    add rate limiter

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const helmet = require("helmet");
 const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
 
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+
 const cookieParser = require("cookie-parser");
 
 app.set("trust proxy", 1);
