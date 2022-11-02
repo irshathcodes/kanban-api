b1f04f8d01d2bbbcc599bd149121af22ff70c3ea
Irshath <irshath700@gmail.com>
2022-11-02 10:51:57 +0530

    add cookie configs for removing cookie

--- utils/removeCookies.js
+++ utils/removeCookies.js
@@ -1,6 +1,18 @@
 const removeCookies = (res) => {
-	res.cookie("accessToken", "logout", { expires: new Date(Date.now()) });
-	res.cookie("refreshToken", "logout", { expires: new Date(Date.now()) });
+	res.cookie("accessToken", "logout", {
+		expires: new Date(Date.now()),
+		signed: true,
+		httpOnly: true,
+		sameSite: "None",
+		secure: process.env.NODE_ENV === "production",
+	});
+	res.cookie("refreshToken", "logout", {
+		expires: new Date(Date.now()),
+		signed: true,
+		httpOnly: true,
+		sameSite: "None",
+		secure: process.env.NODE_ENV === "production",
+	});
 };
 
 module.exports = removeCookies;
